6f05ed748164fd4227705ea1e587b49dc11dab76
hahne <info@sommer-forst.de>
2016-10-28 22:23:18 +0200

    just path fixes

--- Dijkstra/dijkstra_bgl.cc
+++ Dijkstra/dijkstra_bgl.cc
@@ -9,7 +9,6 @@ typedef int64_t    cost_t;
 
 /// From STL library
 #include <fstream>
-
 #include <vector>
 using std::vector;
 
@@ -18,14 +17,32 @@ using std::vector;
 using std::pair;
 using std::make_pair;
 
-#include <boost/graph/graph_traits.hpp> 
-#include <boost/graph/adjacency_list.hpp> 
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/compressed_sparse_row_graph.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
-using namespace boost; 
+#include <boost/graph/dijkstra_shortest_paths_no_color_map.hpp>
 
-typedef adjacency_list < vecS, vecS, directedS, no_property, property<edge_weight_t, cost_t> >   Digraph; 
-typedef graph_traits<Digraph>::vertex_descriptor  Node; 
-typedef graph_traits<Digraph>::edge_descriptor    Arc; 
+using namespace boost;
+
+// Johannes Sommer
+// change graph type at compile time
+//#define USE_ADJACENCY_LIST
+#define USE_COMPRESSED_SPARSE_ROW_GRAPH
+
+// change algorithm at compile time
+//#define USE_DIJKSTRA
+#define USE_DIJKSTRA_NO_COLOR_MAP
+
+#ifdef USE_COMPRESSED_SPARSE_ROW_GRAPH
+typedef compressed_sparse_row_graph < directedS, no_property, property<edge_weight_t, cost_t> >   Digraph;
+#endif
+#ifdef USE_ADJACENCY_LIST
+typedef adjacency_list < vecS, vecS, directedS, no_property, property<edge_weight_t, cost_t> >   Digraph;
+#endif
+
+typedef graph_traits<Digraph>::vertex_descriptor  Node;
+typedef graph_traits<Digraph>::edge_descriptor    Arc;
 
 /// Boost Timer
 #include <boost/progress.hpp>
@@ -33,12 +50,17 @@ using boost::timer;
 
 using namespace boost;
 
+//defines for heap in dijkstra
+//#define BOOST_GRAPH_DIJKSTRA_TESTING
+//#define BOOST_GRAPH_DIJKSTRA_USE_RELAXED_HEAP
+//else: use d-ary heap (d = 4)
+
 /// Read input data, build graph, and run Dijkstra
 cost_t runDijkstra( char* argv[] ) {
    /// Read instance from the OR-lib
-   std::ifstream infile(argv[1]); 
-   if (!infile) 
-      exit ( EXIT_FAILURE ); 
+   std::ifstream infile(argv[1]);
+   if (!infile)
+      exit ( EXIT_FAILURE );
 
    int n;     /// Number of variables
    int m;     /// Number of constraints
@@ -48,30 +70,57 @@ cost_t runDijkstra( char* argv[] ) {
    // e_1 = v_i v_j cost[e_m]
    // ..
    // e_m = v_i v_j cost[e_m]
-   
+
    /// Read the first line
    infile >> n >> m;
    fprintf(stdout,"n %d, m %d\n", n, m);
+
    /// Build the graph
-   Digraph G (n);
-   
    int v, w;
    cost_t c;
+
+#ifdef USE_COMPRESSED_SPARSE_ROW_GRAPH
+
+   vector< pair<int,int> > edges(m);
+   vector<cost_t> costs (m);
+   for ( int i = 0; i < m; i++ ) {
+      infile >> v >> w >> c;
+      edges.push_back(make_pair(v-1,w-1));
+      costs.push_back(c);
+   }
+   Digraph G(boost::edges_are_unsorted, edges.begin(), edges.end(), &costs[0], n);
+
+   //free immediately
+   vector< pair<int,int> > ().swap(edges);
+   vector< cost_t > ().swap(costs);
+
+#else
+   /// Build the graph
+   Digraph G (n);
+
    for ( int i = 0; i < m; i++ ) {
       infile >> v >> w >> c;
       add_edge(v-1, w-1, c, G);
    }
-   
+
+#endif // USE_COMPRESSED_SPARSE_ROW_GRAPH
+
    vector<Node>    P(n);
-   vector<cost_t>  D(n,std::numeric_limits<cost_t>::max());    
+   vector<cost_t>  D(n,std::numeric_limits<cost_t>::max());
    cost_t T_dist;
 
    timer TIMER;
-   for ( int i = 0; i < 50; ++i ) {
+   for ( int i = 0; i < 10; ++i ) {
       double t0 = TIMER.elapsed();
       node_t S = i;
       node_t T = n-1-i;
-      dijkstra_shortest_paths(G, S, predecessor_map(&P[0]).distance_map(&D[0]));
+#ifdef USE_DIJKSTRA_NO_COLOR_MAP
+    dijkstra_shortest_paths_no_color_map(G, S, predecessor_map(&P[0]).distance_map(&D[0]) );
+#endif
+#ifdef USE_DIJKSTRA
+    dijkstra_shortest_paths(G, S, predecessor_map(&P[0]).distance_map(&D[0]));
+#endif
+
       T_dist = D[T];
       fprintf(stdout,"Time %.4f Cost %"PRId64"\n", TIMER.elapsed()-t0, T_dist);
    }
@@ -95,6 +144,7 @@ main (int argc, char **argv)
    cost_t T_dist = runDijkstra(argv);
    /// Print basic figures
    fprintf(stdout,"Cost %" PRId64 " - Time %.3f\n", T_dist, TIMER.elapsed());
-   
+
    return 0;
 }
+
--- Dijkstra/dijkstra_lemon.cc
+++ Dijkstra/dijkstra_lemon.cc
@@ -20,20 +20,86 @@ using std::make_pair;
 
 
 /// Lemon Graph Library
+#include <lemon/list_graph.h>
 #include <lemon/smart_graph.h>
-using lemon::SmartDigraph;
+#include <lemon/static_graph.h>
+#include <lemon/fib_heap.h>
+#include <lemon/quad_heap.h>
+#include <lemon/radix_heap.h>
+#include <lemon/dheap.h>
+#include <lemon/bin_heap.h>
+
+// Johannes Sommer
+// change graph and heap type at compile time
+// Don't optimize too much
+// -O1 gives better results on static graphs
+
+// choose ONE graph type
+//#define USE_LISTGRAPH
+//#define USE_SMARTGRAPH
+#define USE_STATICGRAPH
+
+// choose ONE heap type
+
+//#define USE_BIN_HEAP
+//#define USE_QUAD_HEAP
+//#define USE_FIBONACCI_HEAP
+//#define USE_RADIX_HEAP
+//#define USE_DARY_HEAP_D_2
+#define USE_DARY_HEAP_D_3
+//#define USE_DARY_HEAP_D_4
+//#define USE_DARY_HEAP_D_8
+//#define USE_DARY_HEAP_D_16
+
+
+#ifdef USE_LISTGRAPH
+typedef lemon::ListDigraph graph_t;
+#endif
+#ifdef USE_SMARTGRAPH
+typedef lemon::SmartDigraph graph_t;
+#endif
+#ifdef USE_STATICGRAPH
+typedef lemon::StaticDigraph graph_t;
+#endif
 
 #include <lemon/adaptors.h>
 #include <lemon/concepts/maps.h>
 #include <lemon/dijkstra.h>
 #include <lemon/path.h>
-typedef SmartDigraph::Arc  Arc;
-typedef SmartDigraph::Node Node;
-typedef SmartDigraph::ArcMap<cost_t>   LengthMap;
+typedef graph_t::Arc  Arc;
+typedef graph_t::Node Node;
+typedef graph_t::ArcMap<cost_t>   LengthMap;
+typedef graph_t::NodeMap<int>  NodeMap;
 
-#include <lemon/fib_heap.h>
-typedef SmartDigraph::NodeMap<int>  NodeMap;
-typedef lemon::FibHeap<cost_t, NodeMap> FibonacciHeap;
+#ifdef USE_FIBONACCI_HEAP
+typedef lemon::FibHeap<cost_t, NodeMap> heap_t;
+#endif
+#ifdef USE_QUAD_HEAP
+typedef lemon::QuadHeap<cost_t, NodeMap> heap_t;
+#endif
+#ifdef USE_RADIX_HEAP
+typedef lemon::RadixHeap<NodeMap> heap_t;
+#endif
+#ifdef USE_DARY_HEAP_D_2
+typedef lemon::DHeap<cost_t, NodeMap, 2> heap_t;
+#endif
+#ifdef USE_DARY_HEAP_D_3
+typedef lemon::DHeap<cost_t, NodeMap, 3> heap_t;
+#endif
+#ifdef USE_DARY_HEAP_D_4
+typedef lemon::DHeap<cost_t, NodeMap, 4> heap_t;
+#endif
+#ifdef USE_DARY_HEAP_D_8
+typedef lemon::DHeap<cost_t, NodeMap, 8> heap_t;
+#endif
+#ifdef USE_DARY_HEAP_D_16
+typedef lemon::DHeap<cost_t, NodeMap, 16> heap_t;
+#endif
+
+#ifdef USE_BIN_HEAP
+// default heap type
+typedef lemon::BinHeap<cost_t, NodeMap> heap_t;
+#endif
 
 /// Boost Timer
 #include <boost/progress.hpp>
@@ -44,9 +110,9 @@ using namespace boost;
 /// Read input data, build graph, and run Dijkstra
 cost_t runDijkstra( char* argv[] ) {
    /// Read instance from the OR-lib
-   std::ifstream infile(argv[1]); 
-   if (!infile) 
-      exit ( EXIT_FAILURE ); 
+   std::ifstream infile(argv[1]);
+   if (!infile)
+      exit ( EXIT_FAILURE );
 
    int n;     /// Number of variables
    int m;     /// Number of constraints
@@ -56,12 +122,15 @@ cost_t runDijkstra( char* argv[] ) {
    // e_1 = v_i v_j cost[e_m]
    // ..
    // e_m = v_i v_j cost[e_m]
-   
+
    /// Read the first line
    infile >> n >> m;
    fprintf(stdout,"n %d, m %d\t", n, m);
-   /// Build the graph 
-   SmartDigraph G;
+
+   /// Build the graph
+   graph_t G;
+
+#ifndef USE_STATICGRAPH
    G.reserveNode(n);
    G.reserveArc(m);
    vector<Node> vs;
@@ -71,7 +140,7 @@ cost_t runDijkstra( char* argv[] ) {
 
    int v, w;
    cost_t c;
-   cost_t T_dist; 
+   cost_t T_dist;
    LengthMap    C(G);
    for ( int i = 0; i < m; i++ ) {
       infile >> v >> w >> c;
@@ -79,17 +148,60 @@ cost_t runDijkstra( char* argv[] ) {
       a = G.addArc(vs[v-1], vs[w-1]);
       C[a] = c;
    }
-   
+#else
+    vector<int> vs;
+    vs.reserve(n);
+    for ( int i = 0; i < n; ++i )
+      vs.push_back( i ); //lemon ids start from 1
+
+    int v, w;
+    cost_t c;
+    cost_t T_dist;
+    LengthMap    C(G);
+    //arcs will be built with arcList iterators
+    // populate arcList first
+    vector< pair<int,int> > arcList (m);
+    vector<cost_t> costs (m);
+    for ( int i = 0; i < m; i++ ) {
+      infile >> v >> w >> c;
+      costs.push_back(c);
+      arcList.push_back(make_pair(vs[v-1],vs[w-1]));
+    }
+    G.build(vs.size(), arcList.begin(), arcList.end());
+
+    size_t s = arcList.size();
+
+    //free ressources immediately
+    vector< pair<int,int> > ().swap(arcList);
+
+    //populate costMap
+    for (int i = 0; i < s; i++)
+        C[ G.arc(i) ] = costs[i];
+
+    //free ressources immediately
+    vector<cost_t> ().swap(costs);
+
+#endif // USE_STATICGRAPH
+
    timer TIMER;
-   for ( int i = 0; i < 50; ++i ) {
+   for ( int i = 0; i < 10; ++i ) {
       double t0 = TIMER.elapsed();
+
+#ifndef USE_STATICGRAPH
       Node S = vs[i];
       Node T = vs[n-1-i];
-      //NodeMap heap_cross_ref(G);
-      //FibonacciHeap heap(heap_cross_ref);
-      //lemon::Dijkstra<SmartDigraph, LengthMap>::SetHeap<FibonacciHeap, NodeMap>::Create spp(G, C);
-      //spp.heap( heap, heap_cross_ref );  
-      lemon::Dijkstra<SmartDigraph, LengthMap> spp(G, C);
+#else
+      Node S = G.nodeFromId(vs[i]);
+      Node T = G.nodeFromId(vs[n-1-i]);
+#endif // USE_STATICGRAPH
+
+      //heap change
+      NodeMap heap_cross_ref(G);
+      heap_t heap(heap_cross_ref);
+      lemon::Dijkstra<graph_t, LengthMap>::SetHeap<heap_t, NodeMap>::Create spp(G, C);
+      spp.heap( heap, heap_cross_ref );
+      //else
+      //lemon::Dijkstra<graph_t, LengthMap> spp(G, C);
       spp.run(S,T);
       T_dist = spp.dist(T);
       fprintf(stdout,"Time %.4f Cost %"PRId64"\n", TIMER.elapsed()-t0, T_dist);
